9a8dfb74f995b6c0d26ec9dfaba72e232461272e
jozef rudy <jozef.rudy@gmail.com>
2025-02-28 18:54:32 +0100

    add prettier for auto-formatting

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./components/editor/editor.component').then(
+        (x) => x.EditorComponent,
+      ),
+  },
+];
---
+++ frontend-angular/src/app/components/editor/editor.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editor',
+  imports: [],
+  templateUrl: './editor.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EditorComponent {
+
+}
